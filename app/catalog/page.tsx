b43ba8db7cf70d9e109788297b54ae5a1ceb7aba
Златослава Вишківська <zlatoslava6b@gmail.com>
2026-09-08 20:07:37 +0300

    add font style, css Header, comment on the metadata, install tanstack

--- app/catalog/page.tsx
+++ app/catalog/page.tsx
@@ -1,25 +1,25 @@
-import { Metadata } from "next";
+// import { Metadata } from "next";
 import { QueryClient, HydrationBoundary, dehydrate } from "@tanstack/react-query";
 import { fetchNotes } from "@/lib/api/notes";
 import NotesClient from "./Notes.client";
 
-export const metadata: Metadata = {
-  title: "NoteHub Notes",
-  description: "Browse and manage your notes in NoteHub.",
-  openGraph: {
-    title: "NoteHub Notes",
-    description: "Browse and manage your notes in NoteHub.",
-    url: "https://vercel.com/zlatoslava-vy/08-zustand/settings/domains/notes",
-    images: [
-      {
-        url: "https://ac.goit.global/fullstack/react/notehub-og-meta.jpg",
-        width: 1200,
-        height: 630,
-        alt: "NoteHub Notes",
-      },
-    ],
-  },
-};
+// export const metadata: Metadata = {
+//   title: "RentalCar Catalog",
+//   description: "Choose your perfect rental car from our extensive catalog.",
+//   openGraph: {
+//     title: "RentalCar Catalog",
+//     description: "Choose your perfect rental car from our extensive catalog.",
+//     url: "https://vercel.com/zlatoslava-vy/08-zustand/settings/domains/notes",
+//     images: [
+//       {
+//         url: "https://ac.goit.global/fullstack/react/notehub-og-meta.jpg",
+//         width: 1200,
+//         height: 630,
+//         alt: "NoteHub Notes",
+//       },
+//     ],
+//   },
+// };
 
 export default async function CatalogPage() {
   // Створюємо новий екземпляр QueryClient для серверного запиту
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,42 +1,42 @@
-import type { Metadata } from "next";
-import { Roboto } from "next/font/google";
+// import type { Metadata } from "next";
+import { Manrope } from "next/font/google";
 import { ReactNode } from "react";
 import Header from "@/components/Header/Header";
 import TanStackProvider from "@/components/TanStackProvider/TanStackProvider";
 import "modern-normalize";
 import "./globals.css";
 
-export const metadata: Metadata = {
-  title: "RentalCar",
-  description:
-    "NoteHub: An everyday note-management app featuring category filtering, quick search, and intuitive navigation.",
-  openGraph: {
-    title: "NoteHub",
-    description:
-      "NoteHub: An everyday note-management app featuring category filtering, quick search, and intuitive navigation.",
-    url: "https://08-zustand-tawny-mu.vercel.app/",
-    images: [
-      {
-        url: "https://ac.goit.global/fullstack/react/notehub-og-meta.jpg",
-        width: 1200,
-        height: 630,
-        alt: "NoteHub",
-      },
-    ],
-  },
-};
+// export const metadata: Metadata = {
+//   title: "RentalCar",
+//   description:
+//     "NoteHub: An everyday note-management app featuring category filtering, quick search, and intuitive navigation.",
+//   openGraph: {
+//     title: "NoteHub",
+//     description:
+//       "NoteHub: An everyday note-management app featuring category filtering, quick search, and intuitive navigation.",
+//     url: "https://08-zustand-tawny-mu.vercel.app/",
+//     images: [
+//       {
+//         url: "https://ac.goit.global/fullstack/react/notehub-og-meta.jpg",
+//         width: 1200,
+//         height: 630,
+//         alt: "NoteHub",
+//       },
+//     ],
+//   },
+// };
 
-const roboto = Roboto({
+const manrope = Manrope({
   subsets: ["latin"],
-  weight: ["400", "700"],
-  variable: "--font-roboto",
+  weight: ["500", "600"],
+  variable: "--font-manrope",
   display: "swap",
 });
 
 export default function RootLayout({ children, modal }: { children: ReactNode; modal: ReactNode }) {
   return (
     <html lang="en">
-      <body className={roboto.variable}>
+      <body className={manrope.variable}>
         <TanStackProvider>
           <Header />
           <main className="container">
