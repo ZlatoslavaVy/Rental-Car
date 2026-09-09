99dd48ea7921d37a1cfcf10d629c110011a504bb
Златослава Вишківська <zlatoslava6b@gmail.com>
2026-09-09 12:10:49 +0300

    Conditional object population of cars.service.ts

--- lib/api/cars.service.ts
+++ lib/api/cars.service.ts
@@ -1,11 +1,6 @@
 import type { Car, CarsResponse } from "@/types/car";
 import { api } from "@/lib/api/instance";
 
-export interface FetchCarsResponse {
-  cars: Car[];
-  totalPages: number;
-}
-
 interface FetchCarsParams {
   brand?: string;
   price?: number;
@@ -18,19 +13,28 @@ interface FetchCarsParams {
 export const fetchCars = async (params?: FetchCarsParams) => {
   const queryParams: Record<string, string | number> = {};
 
-  if (params?.tag && params.tag !== "all") {
-    queryParams.tag = params.tag;
+  if (params?.brand) {
+    queryParams.brand = params.brand;
   }
-  if (params?.page) {
-    queryParams.page = params.page;
+  if (params?.price) {
+    queryParams.price = params.price;
   }
+
+  if (params?.minMileage) {
+    queryParams.minMileage = params.minMileage;
+  }
+
+  if (params?.maxMileage) {
+    queryParams.maxMileage = params.maxMileage;
+  }
+
   if (params?.perPage) {
     queryParams.perPage = params.perPage;
   }
 
-  //   if (params?.search) {
-  //     queryParams.search = params.search;
-  //   }
+  if (params?.page) {
+    queryParams.page = params.page;
+  }
 
   const response = await api.get<CarsResponse>("/cars", {
     params: queryParams,
